ad11ed2ec44ba1079af86f49239b31d57d091e73
JD <laeim017@gmail.com>
2019-08-21 14:44:30 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  const title = 'React with Webpack and Babel';
+  return (
+    <>
+      <div>{title}</div>
+      <div>Hello React Project!!</div>
+    </>
+  );
+}
